aae4353a8596e2c0816c44bae178389d22b72e44
Veljko Stevic <stvcvlk@gmail.com>
2019-06-29 23:31:33 +0200

    Fixed account update components

--- components/ChangePassword.vue
+++ components/ChangePassword.vue
@@ -1,59 +1,66 @@
 <template>
-	<div>
-		<!--
-<div class="card">
-					<div class="card-title">Update Password</div>
-					<div class="card-body">
-						<div class="message error" v-if="messages.password.errors.inline_error">{{ messages.password.errors.inline_error }}</div>
-						<div class="message success" v-if="messages.password.success">{{ messages.password.success }}</div>
-
-						<form @submit.prevent="changePassword">
-							<div class="form-group">
-								<label class="form-label">Current Password</label>
-								<input class="form-input" type="password" v-model="password.current_password">	
-								<strong class="danger" v-if="messages.password.errors.current_password">{{ messages.password.errors.current_password[0] }}</strong>
-							</div>
-
-							<div class="form-group">
-								<label class="form-label">New Password</label>
-								<input class="form-input" type="password" v-model="password.password">
-								<strong class="danger" v-if="messages.password.errors.password">{{ messages.password.errors.password[0] }}</strong>
-							</div>
-
-							<div class="form-group">
-								<label class="form-label">Confirm New Password</label>
-								<input class="form-input" type="password" v-model="password.password_confirmation">
-							</div>
-
-							<button class="loading rounded">
-								<div class="spinner"></div>
-								Change Password
-							</button>
-						</form>
-					</div>
+	<div class="card">
+		<div class="card-title">Change Password</div>
+		<div class="card-body">
+			<strong class="success filled" v-if="success">{{ success }}</strong>
+			<strong class="danger filled" v-if="inline_error">{{ inline_error }}</strong>
+			<form @submit.prevent="submit">
+				<div class="form-group">
+					<label class="form-label">Current Password</label>
+					<input class="form-input" type="password" v-model="password.current_password">	
+					<strong class="danger" v-if="errors.current_password">{{ errors.current_password[0] }}</strong>
 				</div>
-		-->
+
+				<div class="form-group">
+					<label class="form-label">New Password</label>
+					<input class="form-input" type="password" v-model="password.password">
+					<strong class="danger" v-if="errors.password">{{ errors.password[0] }}</strong>
+				</div>
+
+				<div class="form-group">
+					<label class="form-label">Confirm New Password</label>
+					<input class="form-input" type="password" v-model="password.password_confirmation">
+				</div>
+
+				<button class="loading rounded">
+					<div class="spinner" v-if="loading"></div>
+					Change Password
+				</button>
+			</form>
+		</div>
 	</div>
 </template>
 
 <script>
-/*
-changePassword() {
+export default {
+	data() {
+	  return {
+	  	password: { current_password: null, password: null, password_confirmation: null, },
+	  	loading: false,
+	  	success: null,
+	  	errors: [],
+	  	inline_error: null,
+	  }
+	},
+	methods: {
+		submit() {
 			this.loading = true;
 			this.$axios.defaults.headers.common['Authorization'] = `Bearer ${this.$store.state.auth.token}`;
 			this.$axios.$post('/me/change-password', this.password).then(response => {
-				this.messages.password.success = response.data;
+				console.log(response)
+				this.errors = [];
+				this.inline_error = null;
+				this.success = response.message;
 				this.loading = false;
 			}).catch(error => {
 				this.loading = false;
-				this.messages.password.errors = [];
-				this.messages.password.errors.inline_error = "";
-				if (error.response.data.message) this.messages.password.errors.inline_error = error.response.data.message;
-				// need small bug fix
-				if (error.response.data.password) this.messages.password.errors = error.response.data;
+				this.errors = [];
+				this.inline_error = null;
+				console.log(error);
+				if (error.response.data.message) this.inline_error = error.response.data.message;
+				if (error.response.data.password) this.errors = error.response.data;
 			});
-		}
+		},
 	}
-*/
-
+}
 </script>
--- pages/profile/update.vue
+++ pages/profile/update.vue
@@ -4,6 +4,7 @@
 
 		<div class="container">
 				<ChangeUserDetails />
+				<ChangePassword />
 		</div>
 	</div>
 </template>
